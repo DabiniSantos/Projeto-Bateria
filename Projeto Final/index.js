503742015225c97a0ce8b3bc336f729f2f9d66cb
DabiniSantos <160979575+DabiniSantos@users.noreply.github.com>
2024-05-30 22:47:02 -0300

    Add files via upload

---
+++ Projeto Final/index.js
@@ -0,0 +1,21 @@
+/* 
+1) Descobrir como detectar os cliques do mouse nos botões.
+
+dica: existe a possibilidade de criar uma função para detectar todos
+ao mesmo tempo.
+
+2) Descobrir como detectar o pressionamento das teclas equivalentes
+no teclado
+
+dica: existe a possibilidade de criar uma função para detectar todos
+os pressionamentos ao mesmo tempo.
+
+3) Criar uma função chamada fazerSom e nessa função, usando laço "if/else" ou "switch/case" para verificar se o usuário clicou a letra
+minúscula ou maiúscula (caps lock ativado). Após a verificação, ao clicar
+o som equivalente a tecla tem que ser acionado.
+
+4) Criar uma função chamada botaoAnimacao para usarmos a estilização
+"pressed" criada para que quando o usuário clicar no botão ele seja 
+estilizado.
+
+*/
